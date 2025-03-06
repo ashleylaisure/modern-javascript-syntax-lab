c06510c3ffd071d50ee48a7c9e9ac8afacfadd39
Ashley Laisure <ashley.laisure@gmail.com>
2025-03-06 10:13:31 -0500

    Completed lab

--- app.js
+++ app.js
@@ -101,3 +101,135 @@ myCar.model = '2001'
 
 console.log(myCar);
 console.log(anotherCar);
+
+// ! Exercise 6: Dynamic Keys in objects
+// a. Define a variable named `propertyName` and assign a string (like 
+//    'username', 'age', or 'email') to it.
+// 
+// b. Create an object named `userProfile`. 
+// 
+// c. Use `propertyName` as a dynamic key in `userProfile`. Assign it a 
+//    relevant value.
+//
+// d. Console log the `userProfile` object to see the result.
+//
+// Your code here:
+
+const propertyName = "username"
+
+const userProfile = {
+  [propertyName] : "Ashley"
+}
+
+console.log(userProfile);
+
+// ! Exercise 8: Default Parameters
+// a. Create a function with two parameters, `noun` and `adjective`.
+// 
+// b. Give `noun` a default value of "cat" and `adjective` a default value of 
+//    "orange".
+//
+// c. The function should log a sentence 'The cat is orange.' by default, but 
+//    should substitute the appropriate parameters when it is supplied with 
+//    arguments.
+//
+// Your code here:
+
+function defaultFun(noun = "cat", adjective = "orange") {
+  return console.log(`The ${noun} is ${adjective}`);
+};
+
+defaultFun();
+
+// ! Exercise 9: Ternary Operator
+// a. Convert the following `if...else` statement into a ternary:
+//
+//    if (pizza === 'tasty') {
+//      console.log('yum');
+//    } else {
+//      console.log('yuck');
+//    }
+//
+// Starting code (don't modify this):
+
+const pizza = 'tasty';
+
+// Your code here:
+
+const goodPizza = pizza === "tasty" ? "Yum" : "Yuck";
+
+console.log(goodPizza);
+
+const myVar = NaN;
+
+const result1 = 'bar' && 'foo';
+const result2 = false || 243;
+const result3 = 42 && false;
+const result4 = myVar || 3000;
+
+console.log('result1:', result1);
+console.log('result2:', result2);
+console.log('result3:', result3);
+console.log('result4:', result4);
+
+// ! Exercise 10:
+// ! 10.1: Set language
+// a. Construct a single line of code that assigns a default value using the 
+//    logical OR operator. This line should match the logic of the following 
+//    statement:
+//
+//    "lang is equal to localLangConfig or the default value of English."
+//
+// b. Create a variable called `lang`.
+//
+// c. Assign `lang` the value of localLangConfig or 'en' as a default if 
+//    `localLangConfig is falsy.
+//
+// d. Log the value of `lang` to the console.
+//
+// Your code here (localLangConfig is provided to get you started):
+
+// Simulated language configuration (change this variable to test)
+const localLangConfig = null ; // Change to 'es', 'fr', etc., or leave it `null`.
+
+const lang = localLangConfig || "en";
+
+console.log(lang);
+
+// ! 10.2: Set website theme
+// Intro: In this exercise, you'll construct a single line of code that assigns 
+//        a default value to a variable named `theme` using the logical OR 
+//        operator. This line should match the logic of the following statement:
+//
+//        "theme is equal to savedUserTheme or the default value of light."
+//
+// a. Create a variable called `theme`.
+//
+// b. Assign `theme` the value of `savedUserTheme` or 'light' as a default.
+//
+// c. Log the value of `theme` to the console.
+//
+// Your code here (`savedUserTheme` is provided to get you started):
+
+// Simulated user theme preference (change this variable to test)
+const savedUserTheme = null; // Change to 'dark', etc., or leave it `null`.
+
+const theme = savedUserTheme || "light"
+
+console.log(theme);
+
+// ! Exercise 11:
+// a. Use optional chaining in a console.log so that a console log of
+//    `adventurer.cat.age` returns `undefined` instead of an error.
+//
+// Starting code (don't modify this):
+
+const adventurer = {
+  name: 'Alice',
+};
+
+// Your code here:
+
+let cat = adventurer.cat?.age
+
+console.log(cat);
